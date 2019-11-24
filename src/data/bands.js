44f165f10fbb878981b5d9e38df8c15623c5fde3
ericwm76 <eric.meldrum@gmail.com>
2019-11-24 16:39:18 -0700

    Copy and paste parsed csv data in bands.js and members.js data files

---
+++ src/data/bands.js
@@ -0,0 +1,499 @@
+const bandsData = [
+  {
+    "band": "NSYNC",
+    "highest_pos": 1,
+    "highest_pos_date": "2000-07-29",
+    "highest_song": "It's Gonna Be Me",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=GQMlWwIXg3M"
+  },
+  {
+    "band": 112,
+    "highest_pos": 4,
+    "highest_pos_date": "2001-07-07",
+    "highest_song": "Peaches And Cream",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=wl2NCXzg1FQ"
+  },
+  {
+    "band": "2gether",
+    "highest_pos": 87,
+    "highest_pos_date": "2000-09-09",
+    "highest_song": "The Hardest Part of Breaking Up",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=HJ89lMXLJGA"
+  },
+  {
+    "band": "5 Seconds of Summer",
+    "highest_pos": 16,
+    "highest_pos_date": "2014-07-19",
+    "highest_song": "Amnesia",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=DCCJCILiX3o"
+  },
+  {
+    "band": "5ive",
+    "highest_pos": 10,
+    "highest_pos_date": "1998-08-01",
+    "highest_song": "When The Lights Go Out",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=mpdcKmaHk_s"
+  },
+  {
+    "band": "98 Degrees",
+    "highest_pos": 2,
+    "highest_pos_date": "2000-09-30",
+    "highest_song": "Give me Just One Night",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=PYb2IdrN-ns"
+  },
+  {
+    "band": "All-4-One",
+    "highest_pos": 1,
+    "highest_pos_date": "1994-05-21",
+    "highest_song": "I Swear",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=25rL-ooWICU"
+  },
+  {
+    "band": "Aventura",
+    "highest_pos": 97,
+    "highest_pos_date": "2005-12-17",
+    "highest_song": "Ella Y Yo",
+    "danceSpeed": "pop",
+    "featuring_artist": "yes",
+    "highest_song_vid": "https://www.youtube.com/watch?v=EZodgl2JiDA"
+  },
+  {
+    "band": "Az Yet",
+    "highest_pos": 8,
+    "highest_pos_date": "1997-05-03",
+    "highest_song": "Hard to Say I'm Sorry (ft. Peter Cetera)",
+    "danceSpeed": "slow",
+    "featuring_artist": "yes",
+    "highest_song_vid": "https://www.youtube.com/watch?v=qpzPrSRQwTw"
+  },
+  {
+    "band": "B2K",
+    "highest_pos": 1,
+    "highest_pos_date": "2003-02-01",
+    "highest_song": "Bump, Bump, Bump (ft. P. Diddy)",
+    "danceSpeed": "pop",
+    "featuring_artist": "yes",
+    "highest_song_vid": "https://www.youtube.com/watch?v=CgiX53hjAPc"
+  },
+  {
+    "band": "Backstreet Boys",
+    "highest_pos": 2,
+    "highest_pos_date": "1997-09-06",
+    "highest_song": "Quit Playing Games with my Heart",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=Ug88HO2mg44"
+  },
+  {
+    "band": "BBMak",
+    "highest_pos": 13,
+    "highest_pos_date": "2000-07-29",
+    "highest_song": "Back Here",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=l95MMbls0GQ"
+  },
+  {
+    "band": "Bell Biv DeVoe",
+    "highest_pos": 3,
+    "highest_pos_date": "1990-06-09",
+    "highest_song": "Poison",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?time_continue=27&v=sb2np1HGqxg"
+  },
+  {
+    "band": "Big Time Rush",
+    "highest_pos": 72,
+    "highest_pos_date": "2011-05-07",
+    "highest_song": "Boyfriend (ft. Snoop Dogg)",
+    "danceSpeed": "pop",
+    "featuring_artist": "yes",
+    "highest_song_vid": "https://www.youtube.com/watch?v=jUOdO5dqPeU"
+  },
+  {
+    "band": "Blackstreet",
+    "highest_pos": 1,
+    "highest_pos_date": "1996-11-09",
+    "highest_song": "No Diggity (ft. Dr. Dre)",
+    "danceSpeed": "pop",
+    "featuring_artist": "yes",
+    "highest_song_vid": "https://www.youtube.com/watch?v=3KL9mRus19o"
+  },
+  {
+    "band": "Boyz II Men",
+    "highest_pos": 1,
+    "highest_pos_date": "1992-08-15",
+    "highest_song": "End of the Road",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=zDKO6XYXioc"
+  },
+  {
+    "band": "Brother Beyond",
+    "highest_pos": 27,
+    "highest_pos_date": "1990-08-25",
+    "highest_song": "The Girl I Used to Know",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=pS-E_lPE668"
+  },
+  {
+    "band": "BTS",
+    "highest_pos": 67,
+    "highest_pos_date": "2017-10-14",
+    "highest_song": "DNA",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=MBdVXkSdhwU"
+  },
+  {
+    "band": "Color Me Badd",
+    "highest_pos": 1,
+    "highest_pos_date": "1991-09-21",
+    "highest_song": "I Adore Mi Amor",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=92NdfSeOLA0"
+  },
+  {
+    "band": "Day26",
+    "highest_pos": 79,
+    "highest_pos_date": "2008-04-12",
+    "highest_song": "Got Me Going",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=jiQaSw2H_LA"
+  },
+  {
+    "band": "Guys Next Door",
+    "highest_pos": 42,
+    "highest_pos_date": "1991-02-23",
+    "highest_song": "I've Been Waiting For You",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=bSArFJxfCeU"
+  },
+  {
+    "band": "Hanson",
+    "highest_pos": 1,
+    "highest_pos_date": "1997-05-24",
+    "highest_song": "MMMBop",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=NHozn0YXAeE"
+  },
+  {
+    "band": "Hi-Five",
+    "highest_pos": 1,
+    "highest_pos_date": "1991-05-18",
+    "highest_song": "I Like The Way",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=vF3MB0U6DO8"
+  },
+  {
+    "band": "Ideal",
+    "highest_pos": 13,
+    "highest_pos_date": "1999-11-20",
+    "highest_song": "Get Gone",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=MPCsH6XiZ_I"
+  },
+  {
+    "band": "Immature",
+    "highest_pos": 5,
+    "highest_pos_date": "1994-10-15",
+    "highest_song": "Never Lie",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=LV8PZ8kcCr8"
+  },
+  {
+    "band": "IMx",
+    "highest_pos": 23,
+    "highest_pos_date": "2000-01-08",
+    "highest_song": "Stay The Night",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=t3s-Vp-Qv5Q"
+  },
+  {
+    "band": "Jagged Edge",
+    "highest_pos": 3,
+    "highest_pos_date": "2001-09-15",
+    "highest_song": "Where The Party At",
+    "danceSpeed": "pop",
+    "featuring_artist": "yes",
+    "highest_song_vid": "https://www.youtube.com/watch?v=9UCY_U4QwqI"
+  },
+  {
+    "band": "Jodeci",
+    "highest_pos": 4,
+    "highest_pos_date": "1993-08-28",
+    "highest_song": "Lately",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=V2ddj5uKbpY"
+  },
+  {
+    "band": "Jonas Brothers",
+    "highest_pos": 5,
+    "highest_pos_date": "2008-07-12",
+    "highest_song": "Burnin' Up",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=5KNEZJ6KkLI"
+  },
+  {
+    "band": "LFO",
+    "highest_pos": 3,
+    "highest_pos_date": "1999-08-28",
+    "highest_song": "Summer Girls",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=NHuGG_FsC20"
+  },
+  {
+    "band": "Menudo",
+    "highest_pos": 62,
+    "highest_pos_date": "1985-06-15",
+    "highest_song": "Hold Me",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=PHRjWauwpvE"
+  },
+  {
+    "band": "Mindless Behavior",
+    "highest_pos": 72,
+    "highest_pos_date": "2011-11-05",
+    "highest_song": "Mrs. Right",
+    "danceSpeed": "pop",
+    "featuring_artist": "yes",
+    "highest_song_vid": "https://www.youtube.com/watch?v=_cuoL8lwaPs"
+  },
+  {
+    "band": "Musical Youth",
+    "highest_pos": 10,
+    "highest_pos_date": "1983-02-26",
+    "highest_song": "Pass the Dutchie",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=dFtLONl4cNc"
+  },
+  {
+    "band": "New Edition",
+    "highest_pos": 3,
+    "highest_pos_date": "1996-08-31",
+    "highest_song": "Hit Me Off",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=fE77mos-ppo"
+  },
+  {
+    "band": "New Kids On The Block",
+    "highest_pos": 1,
+    "highest_pos_date": "1989-06-17",
+    "highest_song": "I'll Be Loving You (Forever)",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=ZT_7UjCVELg"
+  },
+  {
+    "band": "Nine Days",
+    "highest_pos": 6,
+    "highest_pos_date": "2000-07-22",
+    "highest_song": "Absolutely (Story of a Girl)",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=ZIANBamMgas"
+  },
+  {
+    "band": "No Mercy",
+    "highest_pos": 5,
+    "highest_pos_date": "1996-10-12",
+    "highest_song": "Where Do You Go",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=Yt-KMPvgKPo"
+  },
+  {
+    "band": "Nu Flavor",
+    "highest_pos": 27,
+    "highest_pos_date": "1998-01-31",
+    "highest_song": "Heaven",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=9yhHF9_RdJg"
+  },
+  {
+    "band": "O-Town",
+    "highest_pos": 3,
+    "highest_pos_date": "2001-07-28",
+    "highest_song": "All or Nothing",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=TG8IkUoZ6j0"
+  },
+  {
+    "band": "One Direction",
+    "highest_pos": 2,
+    "highest_pos_date": "2013-08-10",
+    "highest_song": "Best Song Ever",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=o_v9MY_FMcw"
+  },
+  {
+    "band": "Perfect Gentlemen",
+    "highest_pos": 10,
+    "highest_pos_date": "1990-06-02",
+    "highest_song": "Ooh La La",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=OAddModv-Kg"
+  },
+  {
+    "band": "Playa",
+    "highest_pos": 38,
+    "highest_pos_date": "1998-06-06",
+    "highest_song": "Cheers 2 U",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=0yvSkYpVZ_I"
+  },
+  {
+    "band": "Shai",
+    "highest_pos": 2,
+    "highest_pos_date": "1992-11-21",
+    "highest_song": "If I Ever Fall in Love",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=caNRc1ER8p4"
+  },
+  {
+    "band": "SILK",
+    "highest_pos": 1,
+    "highest_pos_date": "1993-05-01",
+    "highest_song": "Freak Me",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=Ry6XUsow4Vg"
+  },
+  {
+    "band": "Son by Four",
+    "highest_pos": 26,
+    "highest_pos_date": "2000-08-19",
+    "highest_song": "Purest of Pain (A Puro Dolor)",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=YUGUySygZuU"
+  },
+  {
+    "band": "Soul for Real",
+    "highest_pos": 2,
+    "highest_pos_date": "1995-03-18",
+    "highest_song": "Candy Rain",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=Cx0xzO73Amo"
+  },
+  {
+    "band": "soulDecision",
+    "highest_pos": 22,
+    "highest_pos_date": "2000-09-09",
+    "highest_song": "Faded",
+    "danceSpeed": "pop",
+    "featuring_artist": "yes",
+    "highest_song_vid": "https://www.youtube.com/watch?v=4-SoWeHYH5Q"
+  },
+  {
+    "band": "Take That",
+    "highest_pos": 7,
+    "highest_pos_date": "1995-11-11",
+    "highest_song": "Back for Good",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=N2ICtCO8TCw"
+  },
+  {
+    "band": "The Boys",
+    "highest_pos": 13,
+    "highest_pos_date": "1989-02-25",
+    "highest_song": "Dial My Heart",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=5ZOTtZPr5Ew"
+  },
+  {
+    "band": "The Click Five",
+    "highest_pos": 11,
+    "highest_pos_date": "2005-09-03",
+    "highest_song": "Just The Girl",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=uQBu5whSgC4"
+  },
+  {
+    "band": "The Wanted",
+    "highest_pos": 3,
+    "highest_pos_date": "2012-03-31",
+    "highest_song": "Glad You Came",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=2ggzxInyzVE"
+  },
+  {
+    "band": "Troop",
+    "highest_pos": 47,
+    "highest_pos_date": "1990-08-18",
+    "highest_song": "All I Do Is Think Of You",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=igS9Yv06S3o"
+  },
+  {
+    "band": "U.N.V.",
+    "highest_pos": 2,
+    "highest_pos_date": "1993-07-31",
+    "highest_song": "Something's Goin' On",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=LKYYTIwW7cg"
+  },
+  {
+    "band": "Westlife",
+    "highest_pos": 20,
+    "highest_pos_date": "2000-07-01",
+    "highest_song": "Swear it Again",
+    "danceSpeed": "slow",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=0UaMsIJ4bR0"
+  },
+  {
+    "band": "Youngstown",
+    "highest_pos": 71,
+    "highest_pos_date": "1999-08-21",
+    "highest_song": "I'll Be Your Everything",
+    "danceSpeed": "pop",
+    "featuring_artist": "no",
+    "highest_song_vid": "https://www.youtube.com/watch?v=NprJcyoySFk"
+  }
+];
+
+module.exports = bandsData;
