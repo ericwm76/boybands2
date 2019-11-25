042b750ebccf3ada419388c75a6a0670bd0c9c8d
ericwm76 <eric.meldrum@gmail.com>
2019-11-24 17:57:31 -0700

    Set up server.js and write GET endpoints for all bands and all band members

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/api/v1/bands', (request, response) => {
+  database('bands').select()
+  .then((bands) => {
+    response.status(200).json(bands);
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  });
+});
+
+app.get('/api/v1/members', (request, response) => {
+  database('bandMembers').select()
+  .then((members) => {
+    response.status(200).json(members);
+  })
+  .catch((error) => {
+    response.status(500).json({ error })
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`App is running on ${app.get('port')}`)
+})
